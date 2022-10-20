7d5691f93017bf203ea4f24ba8efd73d3145b0b0
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-10-20 10:25:19 -0500

    Created the list and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
+from . models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {'posts': posts}
+    return render(request, 'blog/post/list.html', context)
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        return Http404('No post found.')
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context)
