db1c698d867f5287a2d7b3e54ef65b3d17a655d8
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-10-24 06:51:11 -0500

    modified the canonical url for posts

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,8 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    #slug would be unique for date
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -37,9 +38,13 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
-    
+        
+        
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                self.publish.month,
+                                                self.publish.day,
+                                                self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator
 from . models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    #pagination with 3 post per page
+    paginator = Paginator(post_list, 3)
     context = {'posts': posts}
     return render(request, 'blog/post/list.html', context)
 
@@ -17,7 +20,12 @@ def post_list(request):
 #     context = {'post': post}
 #     return render(request, 'blog/post/detail.html', context)
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, 
+                            status=Post.Status.PUBLISHED, 
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
     context = {'post': post}
     return render(request, 'blog/post/detail.html', context)
