446982e4474232278f8163d5b2793d3cfa965179
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-10-24 07:46:34 -0500

    Used class based views for post_list

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from . models import Post
 
 
+class PostListView(ListView):
+    """Alternative post list view"""
+    queryset = Post.published.all()
+    context_object_name = 'posts' #the default is object_list e.g post_list
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
+    
 def post_list(request):
     post_list = Post.published.all()
     #pagination with 3 post per page
