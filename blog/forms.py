5c6e6b305d42f4fc94b9351ac5dee768d890b810
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-11-03 00:20:44 -0500

    A comment system

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from . models import Comment
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25, 
@@ -7,3 +9,10 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField(initial='example@gmail.com')  #type='email', value='example.com'
     to = forms.EmailField(help_text='A valid email address please.')
     comments = forms.CharField(required=False, widget=forms.Textarea)  #<textarea></textarea>
+    
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+        # exclude = []
