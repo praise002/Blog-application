50064c3c62a25aeb6e74f0b932971a8f21939ede
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-11-04 12:20:22 -0500

    Custom tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 # Simple tag processes the given data and returns a string
+# Inclusion tags processes the given data and returns a rendered template
 
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()  #used to register the template tag
@@ -9,3 +11,15 @@ register = template.Library()  #used to register the template tag
 @register.simple_tag
 def total_posts():
     return Post.published.count()  #it returns the number of post published in the blog
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}  #it returns a dict of variables
+
+@register.simple_tag
+def get_most_commented_posts(count=5): #count is a default
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
