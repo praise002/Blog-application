026cb650c219b9d1ec70487efe2826cde86f6042
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-11-04 11:45:29 -0500

    similar posts functionality

--- blog/views.py
+++ blog/views.py
@@ -54,10 +54,16 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)  #flat=True gives single values [1, 2, 3, ...]
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     context = {
         'post': post,
         'comments': comments,
-        'form': form
+        'form': form,
+        'similar_posts': similar_posts,
         }
     return render(request, 'blog/post/detail.html', context)
 
