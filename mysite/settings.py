da2dedbb5fd1220983477e2709d257c781874412
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-12-11 15:47:17 -0600

    Updated load_dotenv

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
